08fab929288ec753f5b2d36873b79e3e29a356e6
Kelvin Lo <kel@Kelvins-MacBook-Air.local>
2015-04-10 15:05:09 +0800

    Fix indentation

--- js/boot.js
+++ js/boot.js
@@ -1,35 +1,36 @@
 var bootState = {
-    preload: function () {
-    },
+  preload: function () {
 
-    create: function() {
-        // Set some game settings
-        game.stage.backgroundColor = '#3498db';
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+  },
 
-        // If the device is not a desktop, so it's a mobile device
-        if (!game.device.desktop) {
-            // Set the type of scaling to 'show all'
-            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  create: function() {
+    // Set some game settings
+    game.stage.backgroundColor = '#3498db';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-            // Add a blue color to the page, to hide the white borders we might have
-            document.body.style.backgroundColor = '#3498db';
+    // If the device is not a desktop, so it's a mobile device
+    if (!game.device.desktop) {
+      // Set the type of scaling to 'show all'
+      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-            // Set the min and max width/height of the game
-            game.scale.minWidth = 250;
-            game.scale.minHeight = 170;
-            game.scale.maxWidth = 1000;
-            game.scale.maxHeight = 680;
+      // Add a blue color to the page, to hide the white borders we might have
+      document.body.style.backgroundColor = '#3498db';
 
-            // Center the game on the screen
-            game.scale.pageAlignHorizontally = true;
-            game.scale.pageAlignVertically = true;
+      // Set the min and max width/height of the game
+      game.scale.minWidth = 250;
+      game.scale.minHeight = 170;
+      game.scale.maxWidth = 1000;
+      game.scale.maxHeight = 680;
 
-            // Apply the scale changes
-            game.scale.setScreenSize(true);
-        }
+      // Center the game on the screen
+      game.scale.pageAlignHorizontally = true;
+      game.scale.pageAlignVertically = true;
 
-        // Start the load state
-        game.state.start('load');
+      // Apply the scale changes
+      game.scale.setScreenSize(true);
     }
+
+    // Start the load state
+    game.state.start('load');
+  }
 };
--- js/play.js
+++ js/play.js
@@ -1,25 +1,24 @@
 var playState = {
 
-    create: function() {
-        this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff' });
-        game.global.score = 0;
+  create: function() {
+    this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff' });
+    game.global.score = 0;
 
-        game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(this.tapCount, this);
-        game.input.onDown.add(this.tapCount, this);
-    },
+    game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(this.tapCount, this);
+    game.input.onDown.add(this.tapCount, this);
+  },
 
-    update: function() {
+  update: function() {
 
-    },
+  },
 
-    tapCount: function() {
-      game.global.score ++;
-      this.scoreLabel.text = 'score: ' + game.global.score;
-    },
+  tapCount: function() {
+    game.global.score ++;
+    this.scoreLabel.text = 'score: ' + game.global.score;
+  },
 
-    startMenu: function() {
-        game.state.start('menu');
-    },
+  startMenu: function() {
+    game.state.start('menu');
+  },
 
 };
-
